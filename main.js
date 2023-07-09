4af60b5329261096d28d9443ca139cde13a475a6
Cristian1ve <cristian1ve@gmail.com>
2023-07-08 21:42:44 -0400

    revisión final

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ menuItems.forEach(function(item) {
 });
 
 const changeFontBtn = document.querySelector('#change-font');
-changeFontBtn.addEventListener('click' ,function() {
+changeFontBtn.addEventListener('click',function() {
     document.body.classList.toggle('change-font')
 }
 )
